9377edab94d9f4c3ce4518d69baf48aa47747184
Lavanya <lavanyadeevi0709@gmail.com>
2023-06-16 18:52:56 +0530

    1

--- MemberViewModel .cs
+++ MemberViewModel .cs
@@ -11,7 +11,7 @@ namespace ThinkBridgeUpdated.Models
     public class MemberViewMode
     {
 
-        // Hello 
+        // Hello 1
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         [DataType(DataType.Text)]
