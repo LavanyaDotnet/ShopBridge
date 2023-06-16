69701cd612bab28250a338ab8d952379f4791c51
Lavanya <lavanyadeevi0709@gmail.com>
2023-06-16 18:40:42 +0530

    Hello change comitall

--- MemberViewModel .cs
+++ MemberViewModel .cs
@@ -11,7 +11,7 @@ namespace ThinkBridgeUpdated.Models
     public class MemberViewMode
     {
 
-
+        // Hello changes 
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         [DataType(DataType.Text)]
